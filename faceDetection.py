ca5fdb89d81b43ebb7b37fb86f12a5f717819739
kdc4691 <65882143+kdc4691@users.noreply.github.com>
2020-12-09 16:05:49 +0900

    Create faceDetection.py

---
+++ faceDetection.py
@@ -0,0 +1,12 @@
+  GNU nano 3.2                    faceDetection.py                              
+
+    img = cv2.flip(img, -1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(20, 20)
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
